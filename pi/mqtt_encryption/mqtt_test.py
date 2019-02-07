8b14a0104b4ba538dd433f737e6ebe6de6971710
yuzhuo tan <tanyz1997@gmail.com>
2019-02-07 10:40:49 +0000

    encrypt message from pi

--- pi/mqtt_encryption/mqtt_test.py
+++ pi/mqtt_encryption/mqtt_test.py
@@ -4,10 +4,10 @@ client = mqtt.Client()
 
 client.tls_set(ca_certs="mosquitto.org.crt", certfile="client.crt", keyfile="client.key")
 
-code = client.connect("test.mosquitoo.org", port=8884)
+code = client.connect("test.mosquitto.org", port=8884)
 
 print("Connection code:", code)
 
-code = client.publish("IC.embedded/Internet_of_Tings/test", "hello")
+code = client.publish("IC.embedded/internet_of_tings/test", "hello")
 
 print("Publish code: ", code)
